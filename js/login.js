8e04161ff8a760875b9eabd07c41e08778fc9444
Supermonters <94298972+Supermonters@users.noreply.github.com>
2023-01-07 23:49:30 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,10 +3,12 @@ function load() {
     return;
   } else {
     var a = localStorage.getItem("TaiKhoanDangNhap");
-    let b = document.getElementsByClassName("welcomeUser");
-    for (let i = 0; i < b.length; i++)
-      b[i].textContent = `Welcome, ${a}`;
-  }
+    for (let i = 0; i < 3; i++) {
+      let b = document.getElementsByClassName("welcomeUser")[i].innerHTML= `Welcome, ${a}` ;      
+    }
+  //   for (let i = 0; i < b.length; i++)
+  //     b[i].textContent = `Welcome, ${a}`;
+}
 }
 function cartCounting() {
   var cartItemNumber = 0;
