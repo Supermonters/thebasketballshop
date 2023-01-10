872c86f2f2a3930f6682cf2be4076c7ca3b2e9f6
pleasenoban <plsnoban2022@gmail.com>
2023-01-10 17:13:13 +0700

    date thing

--- js/news.js
+++ js/news.js
@@ -1,16 +1,20 @@
 (async () => {
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
   let a = await fetch(
-    "https://newsdata.io/api/1/news?apikey=pub_1550576968a334f90cbb606bd4690ca338b1e&q=nba&language=en&category=sports "
+    "https://newsdata.io/api/1/news?apikey=pub_1550576968a334f90cbb606bd4690ca338b1e&q=nba&language=en&category=sports"
   );
   let data = await a.json();
-  let arr = data.results.filter((el) => el.image_url !== null);
-  console.log(arr.length);
+  let arr = data.results.filter((el) => (el.image_url !== null && el.description !== null));
+  let d = new Date();
   for (let i = 0; i < arr.length; i++) {
+    d.setTime(Date.parse(arr[i].pubDate));
     let html = `
     <div class="post">
     <div class="entry-date">
-        <div class="date">18</div>
-        <span class="month">Sep</span>
+        <div class="date">${d.getDate()}</div>
+        <span class="month">${months[d.getMonth()]}</span>
     </div>
     <div class="featured-image">
         <img src="${arr[i].image_url}"
